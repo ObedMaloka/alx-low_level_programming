7827bc42a8ead206eb2e53800c7db6710f946d7f
Obed Maloka <62782592@mylife.unisa.ac.za>
2023-02-17 17:53:42 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/** main - Entry point
+ * Description: Print alphabets in lowercase, except q & e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		if (alphabets != 'q' && alphabets != 'e')
+			putchar(alphabets);
+	}
+	putchar('\n');
+
+	return (0);
+}
