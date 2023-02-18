e523235b4e174e79563b18c92b82e9437136c590
Obed Maloka <62782592@mylife.unisa.ac.za>
2023-02-17 18:58:28 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/** main - Entry point
+ * Description: Prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+	char alphabets;
+
+	while (a < 10)
+	{
+		putchar('0' + a);
+		a++;
+	}
+	for (alphabets = 'a'; alphabets <= 'f'; alphabets++)
+	{
+                        putchar(alphabets);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
