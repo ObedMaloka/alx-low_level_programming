5b11a8071f85821f1e8b22432337e5a4fa209c22
Obed Maloka <62782592@mylife.unisa.ac.za>
2023-02-21 13:14:34 -0800

    4-isalpha.c exercise

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for lowercase & uppercase letters
+ * @c: Parameter checked for lowercase & uppercase
+ * Return: Return (1) if @c is lowercase or uppercase, otherwise (0)
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'B'))
+		return (1);
+	else
+		return (0);
+}
