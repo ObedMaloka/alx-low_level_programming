a2cf963ecbe391f2162aa72e9790632dc1c0cd14
Obed Maloka <62782592@mylife.unisa.ac.za>
2023-02-21 01:13:14 -0800

    0-putchar.c exercise

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/**
+ * Below is main.h header file that has
+ * a prototype calledd in the main function
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints "_putchar" using the _putchar function it's body
+ * Return: Returns (0) after execution
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
