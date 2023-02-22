93340bcc614a37bbfe365c5cbfd8e1a04da3e65f
Obed Maloka <62782592@mylife.unisa.ac.za>
2023-02-21 17:37:11 -0800

    10-add.c exercise

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/** add - Adds two integers and returns the result
+ * @a: First parameter
+ * @b: Second parameter
+ * Return: Returns result of add function
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
+
