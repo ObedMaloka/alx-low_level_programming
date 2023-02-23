776750824d224ddf3f2e8b24e42bf98a10c4aafd
Obed Maloka <62782592@mylife.unisa.ac.za>
2023-02-22 23:51:54 -0800

    2-mul.c exercise

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers a & b
+ * @a: First parameter
+ * @b: Second parameter
+ * Return: Returns the product of multiplying a & b
+ */
+
+int mul(int a, int b)
+{
+	int productOfMultiplication = a * b;
+	return (productOfMultiplication);
+}
