8d9cab280044a20f1012f8e9cf7b0dd13495a862
Obed Maloka <62782592@mylife.unisa.ac.za>
2023-02-28 05:10:28 -0800

    0-reset_to_98.c exercise

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *reset_to_98 - Takes a pointer to an int as parameter and
- *updates the value it points to to 98
- *@n: Pointer to int parameter that updates value
- *Return: 0 always
+ * reset_to_98 - Takes a pointer to an int as parameter and
+ * updates the value it points to to 98
+ * @n: Parameter to be updated
+ * Return: void
  */
 
 void reset_to_98(int *n)
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <unistd.h>
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,6 +13,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-int _putchar(char c);
 
 #endif
